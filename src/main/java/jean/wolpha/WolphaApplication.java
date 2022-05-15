d21989e8bfaec031d03d1eaabb44f456fc82c6a4
Chanyut Yuvacharuskul <chanyut22@hotmail.com>
2022-05-15 10:57:03 +0700

    spring init

---
+++ src/main/java/jean/wolpha/WolphaApplication.java
@@ -0,0 +1,13 @@
+package jean.wolpha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WolphaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WolphaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jean/wolpha/WolphaApplicationTests.java
@@ -0,0 +1,13 @@
+package jean.wolpha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WolphaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
