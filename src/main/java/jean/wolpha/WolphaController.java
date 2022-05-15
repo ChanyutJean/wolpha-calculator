1bbd8bcbd62e2857c93e3ff37b6d1804451bc64b
Chanyut Yuvacharuskul <chanyut22@hotmail.com>
2022-05-15 16:12:14 +0700

    working api

--- src/main/java/jean/wolpha/WolphaController.java
+++ src/main/java/jean/wolpha/WolphaController.java
@@ -1,5 +1,6 @@
 package jean.wolpha;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,11 @@ import java.math.BigDecimal;
 
 @RestController
 public class WolphaController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to Wolpha! To use this API, please perform a POST request on this endpoint.";
+    }
 
     @PostMapping("/")
     public BigDecimal calculate(@RequestBody String expr) {
