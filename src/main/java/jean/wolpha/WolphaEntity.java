d2d4ae12a9b7a68a01ddabdd5782433e56a9ac66
Chanyut Yuvacharuskul <chanyut22@hotmail.com>
2022-06-01 17:40:31 +0700

    postgresql for heroku

--- src/main/java/jean/wolpha/WolphaEntity.java
+++ src/main/java/jean/wolpha/WolphaEntity.java
@@ -7,7 +7,7 @@ import javax.persistence.*;
 import java.sql.Timestamp;
 
 @Entity
-@Table(name = "entity", schema = "wolpha")
+@Table(name = "entity")
 @Setter
 @NoArgsConstructor
 public class WolphaEntity {
